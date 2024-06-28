cbcd32848ebb58bb5c199293b970d4db97fed6fd
mozxammil <mozxammil>
2024-06-28 23:44:00 +0530

    adding navbar

---
+++ client/src/components/ui/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="fixed top-0 left-0 w-full z-[999] flex items-center justify-between px-10 py-5 text-white">
+      <div>
+        <h1 className="text-3xl font-bold font-[Sytle-Script] ">
+          Adharsh
+        </h1>
+      </div>
+      <div>ritgh</div>
+    </nav>
+  );
+};
+
+export default Header;
